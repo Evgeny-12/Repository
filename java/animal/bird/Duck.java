4577211e03dbd324d7e833bfc4319cee53b1e407
Evgeny <e_kvashnin97@mail.ru>
2024-03-12 14:20:04 +0300

    commit1

--- java/animal/bird/Duck.java
+++ java/animal/bird/Duck.java
@@ -4,12 +4,7 @@ import animal.pets.Animal;
 
 public class Duck extends Animal implements Flying {
 
-    public Duck() {
-    }
 
-    public Duck(String name, int age, double weight, String color) {
-        super();
-    }
 
     @Override
     public void say() {
--- java/animal/pets/Dog.java
+++ java/animal/pets/Dog.java
@@ -2,12 +2,7 @@ package animal.pets;
 
 public class Dog extends Animal {
 
-    public Dog() {
-    }
 
-    public Dog(String name, int age, double weight, String color) {
-        super();
-    }
 
     @Override
     public void say() {
